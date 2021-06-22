176359bc751feca4c5cbaf635f3d1091074f641b
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-06-22 14:02:40 +0400

    feat(slider) add auto slide

--- slider/script.js
+++ slider/script.js
@@ -3,13 +3,50 @@ let slideRight = document.querySelector(".slider__button-right");
 let slider = document.querySelector(".slider-main");
 let sliderItems = document.querySelectorAll(".slider-item");
 
+let intervalSlide = setInterval(slideRightFunction, 3000);
+
+function slideRightFunction() {
+  slider.firstElementChild.style.opacity = 0.6;
+  document.querySelectorAll(".slider-item")[1].style.opacity = 0.6;
+  setTimeout(() => {
+    slider.insertBefore(
+      slider.firstElementChild,
+      sliderItems[sliderItems.length]
+    );
+    requestAnimationFrame(() => {
+      slider.firstElementChild.style.opacity = 1;
+    });
+  }, 250);
+}
+
 slideLeft.addEventListener("click", () => {
-  slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+  clearInterval(intervalSlide);
+  slider.firstElementChild.style.opacity = 0.6;
+  slider.lastElementChild.style.opacity = 0.6;
+  setTimeout(() => {
+    slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+    requestAnimationFrame(() => {
+      slider.firstElementChild.style.opacity = 1;
+    });
+  }, 250);
+
+  intervalSlide = setInterval(slideRightFunction, 3000);
 });
 
 slideRight.addEventListener("click", () => {
-  slider.insertBefore(
-    slider.firstElementChild,
-    sliderItems[sliderItems.length]
-  );
+  clearInterval(intervalSlide);
+
+  slider.firstElementChild.style.opacity = 0.6;
+  document.querySelectorAll(".slider-item")[1].style.opacity = 0.6;
+  setTimeout(() => {
+    slider.insertBefore(
+      slider.firstElementChild,
+      sliderItems[sliderItems.length]
+    );
+    requestAnimationFrame(() => {
+      slider.firstElementChild.style.opacity = 1;
+    });
+  }, 250);
+
+  intervalSlide = setInterval(slideRightFunction, 3000);
 });
